fbab5f2cfbd8609bb8c3060393039ee21f202490
Adam Maas <adamjm1220@gmail.com>
2024-12-30 12:37:51 -0500

    Added S3Access ROle Module for Databricks

---
+++ modules/databrickss3roles/main.tf
@@ -0,0 +1,47 @@
+resource "aws_iam_role" "databricks_s3_access" {
+  name = "DatabricksS3Access"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "databricks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# IAM Policy for Specific Buckets
+resource "aws_iam_policy" "databricks_s3_limited_access" {
+  name = "DatabricksS3LimitedAccessPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*" # Allows all S3 actions
+        ],
+        Resource = [
+          "arn:aws:s3:::raws3bucket-databricks-1220-16492640",   
+          "arn:aws:s3:::raws3bucket-databricks-1220-16492640/*", 
+          "arn:aws:s3:::trusteds3bucket-databricks-1220-16492640",    
+          "arn:aws:s3:::trusteds3bucket-databricks-1220-16492640/*",   
+          "arn:aws:s3:::refineds3bucket-databricks-1220-16492640",  
+          "arn:aws:s3:::refineds3bucket-databricks-1220-16492640/*" 
+        ]
+      }
+    ]
+  })
+}
+
+# Attach the Policy to the Role
+resource "aws_iam_role_policy_attachment" "databricks_s3_access_attach" {
+  role       = aws_iam_role.databricks_s3_access.name
+  policy_arn = aws_iam_policy.databricks_s3_limited_access.arn
+}
