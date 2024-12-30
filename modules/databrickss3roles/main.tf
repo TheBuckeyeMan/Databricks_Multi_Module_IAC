0e96b4be4f0b33fcdeb1887a1cb6c8e8ac4b93a5
Adam Maas <adamjm1220@gmail.com>
2024-12-30 14:31:30 -0500

    Added S3Access Role Module for Databricks

--- modules/databrickss3roles/main.tf
+++ modules/databrickss3roles/main.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "databricks_s3_access" {
       {
         Effect = "Allow",
         Principal = {
-            Service = "databricks.amazonaws.com"
+            "AWS": "arn:aws:iam::339712758982:role/eb28771d-297d-4987-b323-47d4a8b2114e"
         },
         Action = "sts:AssumeRole"
       }
